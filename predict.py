993400aea95396c41c2fc3d9b8f507ab920dd57c
Mitchel Deken <mitcheldeken@live.nl>
2023-11-13 11:07:02 +0100

    changed predict.py

--- predict.py
+++ predict.py
@@ -10,21 +10,6 @@ import numpy as np
 import imutils
 import json
 
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    
-    ref = anns[0]['segmentation']
-
-    canvas = np.zeros((ref.shape[0], ref.shape[1], 3))
-
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.random.random((1, 3))
-        canvas[m] = np.uint8(color_mask*255)
-    return canvas
-
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
@@ -37,7 +22,7 @@ class Predictor(BasePredictor):
     def predict(
         self,
         image: Path = Input(description="Input image"),
-        resize_width: int = Input(default=1024, description="The width to resize the image to before running inference."),
+        resize_width: int = Input(default=0, description="The width to resize the image to before running inference. Use 0 if you don't want resizing"),
         points_per_side: int = Input(default = 32, description= "The number of points to be sampled along one side of the image. The total number of points is points_per_side**2. If None, point_grids must provide explicit point sampling."),
         pred_iou_thresh: float = Input(default=0.88,description="A filtering threshold in [0,1], using the model's predicted mask quality."),
         stability_score_thresh: float = Input(default=0.95, description="A filtering threshold in [0,1], using the stability of the mask under changes to the cutoff used to binarize the model's mask predictions."),
@@ -48,7 +33,7 @@ class Predictor(BasePredictor):
         crop_overlap_ratio: float = Input(default=512 / 1500, description= "Sets the degree to which crops overlap. In the first crop layer, crops will overlap by this fraction of the image length. Later layers with more crops scale down this overlap."),
         crop_n_points_downscale_factor: int = Input(default=1, description= "The number of points-per-side sampled in layer n is scaled down by crop_n_points_downscale_factor**n."),
         min_mask_region_area: int = Input(default=0, description="If >0, postprocessing will be applied to remove disconnected regions and holes in masks with area smaller than min_mask_region_area."),
-        output_mode: str = Input(default="uncompressed_rle", description="output format")
+        output_mode: str = Input(default="uncompressed_rle", description="output format. Can be 'uncompressed_rle', 'coco_rle' or 'binary_mask'")
         ) -> Path:
         """Run a single prediction on the model"""
         args = locals()
@@ -59,12 +44,10 @@ class Predictor(BasePredictor):
         mask_generator = SamAutomaticMaskGenerator(self.sam, **args)
 
         image = cv2.imread(str(image))
-        image = imutils.resize(image, width=resize_width)
-
+        if resize_width>0:
+            image = imutils.resize(image, width=resize_width)
         masks = mask_generator.generate(image)
-        #annotations = show_anns(masks)
-
-        #cv2.imwrite("output_mask.png", annotations)
+   
         with open('masks.json', 'w') as file:
             json.dump(masks, file)
 
